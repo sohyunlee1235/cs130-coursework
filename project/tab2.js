32badcd5d74056924765b5a01855885c8ea24d0c
Sohyun Lee <sohyunlee1235@gmail.com>
2021-05-18 09:17:02 -0500

    Checking in my project

---
+++ project/tab2.js
@@ -0,0 +1,11 @@
+const clickParagraph = (ev) => {
+    const text = document.querySelector("p").innerHTML;   
+    const my_string = text[0]+text[1]+text[2] ;
+    if (my_string === '<i>') {
+        // console.log('hi');
+        document.querySelector("p").innerHTML="";
+    };
+};
+
+document.querySelector("p").onclick = clickParagraph;
+
