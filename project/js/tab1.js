e5e6819e14d2909565abe8b113e283c5581907b1
Sohyun Lee <sohyunlee1235@gmail.com>
2021-05-28 18:19:14 -0500

    Project 2nd deliverable

--- project/js/tab1.js
+++ project/js/tab1.js
@@ -1,8 +1,51 @@
-// Figure out how to
-// 0. incorporate time widgets
-// 1. click + enter key to create bullet points -- customize bullets with checkboxes
-// 2. tab key to indent bullets
-// 3. save -- Ctrl+S AND click button to save
-// 4. hover effect for each box: background turns light grey
+
+const create_new_bullet_first = (ev) => {
+    var old_text = document.querySelector('#first_list').innerHTML;
+    var original_text = old_text.split("<p")[0];
+    document.querySelector('#first_list').innerHTML = 
+        `${original_text}` + `<li></li>` + `<p contenteditable="false" id="more" onclick="create_new_bullet_first(event)">
+        <span>&#43;</span>
+        </p>`
+}
+
+const create_new_bullet_second = (ev) => {
+    var old_text = document.querySelector('#second_list').innerHTML;
+    var original_text = old_text.split("<p")[0];
+    document.querySelector('#second_list').innerHTML = 
+        `${original_text}` + `<li></li>` + `<p contenteditable="false" id="more" onclick="create_new_bullet_second(event)">
+        <span>&#43;</span>
+        </p>`
+}
+
+const save_edits = () => {
+    var edited_text = document.querySelector('.bulleted_list').innerHTML;
+    localStorage.tab1Edits = edited_text;
+    alert('Edits saved!');
+}
+
+const check_edits = () => {
+    if (localStorage.tab1Edits != null) {
+        document.querySelector('.bulleted_list').innerHTML = localStorage.tab1Edits;
+    }
+}
+
+const tab = (ev) => {
+    // Number 9 is the "tab" key on the keyboard -- trying to indent;
+    console.log(ev.keyCode);
+    if (ev.keyCode == 9) {
+        ev.preventDefault();
+    }
+};
+
+const backspace = (ev) => {
+    console.log('hi!');
+    if (ev.keyCode == 8) {
+        console.log(ev.currentTarget);
+        // var text = ev.currentTarget.innerHTML.split("<p")[0];
+        // console.log(text);
+        // document.querySelector('')
+        // if (text == '<li>')
+    }
+}
 
 
--- project/js/tab2.js
+++ project/js/tab2.js
@@ -1,10 +1,43 @@
 const clickParagraph = (ev) => {
-    const text = document.querySelector("p").innerHTML;   
+    const text = document.querySelector("#paragraph").innerHTML;   
     const my_string = text[0]+text[1]+text[2] ;
     if (my_string === '<i>') {
-        document.querySelector("p").innerHTML="";
+        document.querySelector("#paragraph").innerHTML="";
     };
+}
+
+const create_new_bullet_first = (ev) => {
+    var old_text = document.querySelector('#first_list').innerHTML;
+    var original_text = old_text.split("<p")[0];
+    document.querySelector('#first_list').innerHTML = 
+        `${original_text}` + `<li></li>` + `<p contenteditable="false" id="more" onclick="create_new_bullet_first(event)">
+        <span>&#43;</span>
+        </p>`
+}
+
+const tab = (ev) => {
+    // Number 9 is the "tab" key on the keyboard -- trying to indent;
+    console.log(ev.keyCode);
+    if (ev.keyCode == 9) {
+        ev.preventDefault();
+    }
+    // if (ev.keyCode == )
 };
 
-document.querySelector("p").onclick = clickParagraph;
+const save_edits = () => {
+    var edited_text_list = document.querySelector('.bulleted_list').innerHTML;
+    var edited_text_par = document.querySelector('.paragraph').innerHTML;
+    localStorage.tab2_listEdits = edited_text_list;
+    localStorage.tab2_parEdits = edited_text_par;
+    alert('Edits saved!');
+}
+
+const check_edits = () => {
+    if (localStorage.tab2_listEdits != null) {
+        document.querySelector('.bulleted_list').innerHTML = localStorage.tab2_listEdits;
+    }
+    if (localStorage.tab2_parEdits != null) {
+        document.querySelector('.paragraph').innerHTML = localStorage.tab2_parEdits;
+    }
+}
 
